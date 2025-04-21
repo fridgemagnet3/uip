6c740573c19a97e175fa500ade10160a3bcb3a2a
jon bird <jon@onasticksoftware.co.uk>
2025-04-21 14:44:57 +0100

    Fix more warnings

--- apps/webserver/httpd-fsdata.c
+++ apps/webserver/httpd-fsdata.c
@@ -582,25 +582,25 @@ static const unsigned char data_stats_shtml[] = {
 	0x6f, 0x6f, 0x74, 0x65, 0x72, 0x2e, 0x68, 0x74, 0x6d, 0x6c, 
 	0xa, 0};
 
-const struct httpd_fsdata_file file_processes_shtml[] = {{NULL, data_processes_shtml, data_processes_shtml + 17, sizeof(data_processes_shtml) - 17}};
+const struct httpd_fsdata_file file_processes_shtml[] = {{NULL, (const char*)data_processes_shtml, (const char*)data_processes_shtml + 17, sizeof(data_processes_shtml) - 17}};
 
-const struct httpd_fsdata_file file_404_html[] = {{file_processes_shtml, data_404_html, data_404_html + 10, sizeof(data_404_html) - 10}};
+const struct httpd_fsdata_file file_404_html[] = {{file_processes_shtml, (const char*)data_404_html, (const char*)data_404_html + 10, sizeof(data_404_html) - 10}};
 
-const struct httpd_fsdata_file file_files_shtml[] = {{file_404_html, data_files_shtml, data_files_shtml + 13, sizeof(data_files_shtml) - 13}};
+const struct httpd_fsdata_file file_files_shtml[] = {{file_404_html, (const char*)data_files_shtml, (const char*)data_files_shtml + 13, sizeof(data_files_shtml) - 13}};
 
-const struct httpd_fsdata_file file_footer_html[] = {{file_files_shtml, data_footer_html, data_footer_html + 13, sizeof(data_footer_html) - 13}};
+const struct httpd_fsdata_file file_footer_html[] = {{file_files_shtml, (const char*)data_footer_html, (const char*)data_footer_html + 13, sizeof(data_footer_html) - 13}};
 
-const struct httpd_fsdata_file file_header_html[] = {{file_footer_html, data_header_html, data_header_html + 13, sizeof(data_header_html) - 13}};
+const struct httpd_fsdata_file file_header_html[] = {{file_footer_html, (const char*)data_header_html, (const char*)data_header_html + 13, sizeof(data_header_html) - 13}};
 
-const struct httpd_fsdata_file file_index_html[] = {{file_header_html, data_index_html, data_index_html + 12, sizeof(data_index_html) - 12}};
+const struct httpd_fsdata_file file_index_html[] = {{file_header_html, (const char*)data_index_html, (const char*)data_index_html + 12, sizeof(data_index_html) - 12}};
 
-const struct httpd_fsdata_file file_style_css[] = {{file_index_html, data_style_css, data_style_css + 11, sizeof(data_style_css) - 11}};
+const struct httpd_fsdata_file file_style_css[] = {{file_index_html, (const char*)data_style_css, (const char*)data_style_css + 11, sizeof(data_style_css) - 11}};
 
-const struct httpd_fsdata_file file_tcp_shtml[] = {{file_style_css, data_tcp_shtml, data_tcp_shtml + 11, sizeof(data_tcp_shtml) - 11}};
+const struct httpd_fsdata_file file_tcp_shtml[] = {{file_style_css, (const char*)data_tcp_shtml, (const char*)data_tcp_shtml + 11, sizeof(data_tcp_shtml) - 11}};
 
-const struct httpd_fsdata_file file_fade_png[] = {{file_tcp_shtml, data_fade_png, data_fade_png + 10, sizeof(data_fade_png) - 10}};
+const struct httpd_fsdata_file file_fade_png[] = {{file_tcp_shtml, (const char*)data_fade_png, (const char*)data_fade_png + 10, sizeof(data_fade_png) - 10}};
 
-const struct httpd_fsdata_file file_stats_shtml[] = {{file_fade_png, data_stats_shtml, data_stats_shtml + 13, sizeof(data_stats_shtml) - 13}};
+const struct httpd_fsdata_file file_stats_shtml[] = {{file_fade_png, (const char*)data_stats_shtml, (const char*)data_stats_shtml + 13, sizeof(data_stats_shtml) - 13}};
 
 #define HTTPD_FS_ROOT file_stats_shtml
 
